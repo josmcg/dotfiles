a3cdd4cfb6b2e17d6e878a8d03c6fc95750153e0
Josh McGrath <joshuawmcgrath@gmail.com>
2019-01-16 10:57:47 -0600

    cleanup

--- .config/google-chrome/Default/Extensions/ojocioejjpmeccjfjlhkepchminjemod/0.7.6_0/main.js
+++
@@ -1,7 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create(
-      "window.html",
-      {
-        innerBounds: { width: 230, height: 420, minWidth: 230}
-      });
-});
